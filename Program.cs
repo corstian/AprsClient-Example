cef84f1b69ef23c777db397c424b608abeadf267
Corstian Boerman <mail@corstianboerman.com>
2017-09-17 12:46:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using Boerman.Aeronautics.AprsClient;
+
+namespace Boerman.Aeronautical.AprsMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "APRS Console";
+
+            Listener listener = new Listener();
+
+            StreamWriter w = File.AppendText("positions.txt");
+            w.AutoFlush = true;
+
+            listener.PacketReceived += (sender, eventArgs) =>
+            {
+                // Please note it is faster to use the `listener.DataReceived` event if you only want the raw data.
+                Console.WriteLine(eventArgs.AprsMessage.RawData);
+                w.Write($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture)} {eventArgs.AprsMessage.RawData}");
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Boerman.Aeronautical.AprsMonitor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Boerman.Aeronautical.AprsMonitor")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd74a15e-3b91-427d-8636-1dff7a59393d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.81")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
